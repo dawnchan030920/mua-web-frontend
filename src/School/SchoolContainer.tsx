91c1de4c368428b473ce3b798cff6663939d9379
Dawn Chan <dawnchan030920@outlook.com>
2022-10-18 10:34:50 +0800

    Fix SchoolContainer.

--- src/School/SchoolContainer.tsx
+++ src/School/SchoolContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-const SchoolContainer: React.FX<{}> = () => {
+const SchoolContainer: React.FC<{}> = () => {
   return (
     <>
       <div>Now you have entered school stuff.</div>
