8365debcc0d29d45aa0720dc17531803337dbd52
Dawn Chan <dawnchan030920@outlook.com>
2022-10-28 16:50:59 +0800

    Init axios & test api connection.

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,238 +1,41 @@
 import React, { useState } from "react";
 import ArticleLayout from "../../components/Layout/ArticleLayout";
 import { ReactComponent as Home24 } from "../../assets/icons/home24.svg";
+import useAxios from "axios-hooks";
 
 const HomePage: React.FC<{}> = () => {
+  const [{data, loading, error}, refetch] = useAxios(
+    "/api/passage/getPassageList/2"
+  );
+
+  if (loading) return <div>Loading</div>
+	if (error) return <p>Error</p>
+
   return (
-    <ArticleLayout
-      title="Home Test Article"
-      passageTitles={[
-        {
-          header: "This is header 1",
-          level: 2,
-          id: "header1",
-        },
-        {
-          header: "This is header 2",
-          level: 2,
-          id: "header2",
-        },
-      ]}
-      categoryLink="/"
-      categoryIcon={<Home24 />}
-    >
-      <h1 id="header1">This is header1</h1>
-      <p>
-        何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸何青逸
-      </p>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br /> <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <h2 id="header2">This is header2</h2>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br /> <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p>This is a test paragraph.</p>
-    </ArticleLayout>
+    <>
+      <ArticleLayout
+        title="Home Test Article"
+        passageTitles={[
+          {
+            header: "This is header 1",
+            level: 2,
+            id: "header1",
+          },
+          {
+            header: "This is header 2",
+            level: 2,
+            id: "header2",
+          },
+        ]}
+        categoryLink="/"
+        categoryIcon={<Home24 />}
+      >
+        <h2 id="header1">This is header1</h2>
+        <h2 id="header2">This is header2</h2>
+
+				<pre>{JSON.stringify(data, null, 2)}</pre>
+      </ArticleLayout>
+    </>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    port: 8000,
+    open: false,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:30001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
